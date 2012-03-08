7d01818e6d05533ff7489bc7e15a1b3bd5bdafbd
genius@s146.net.ru <genius@s146.net.ru@f72ce51f-2800-f434-ffef-dc6886c108de>
2012-03-08 06:14:28 +0000

    Fresh version

--- trunk/firmware/propeller_clock/propeller_clock.c
+++ trunk/firmware/propeller_clock/propeller_clock.c
@@ -6,16 +6,21 @@
  */ 
 
 #define F_CPU 8000000UL // Stub
+// high and low ADC values to turn on/off power dumper
+#define Dhigh 702
+#define Dlow  526
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <inttypes.h>
+#include <stdint.h>
 
 /* Declarations */
 void init();
 void led_output( int16_t line);
 
 /* Timings for pwm */
-#define SMALL_INT 1250 // 10ms
+#define SMALL_INT 125 // 10ms
 #define BIG_INT 12500 // 100ms
 
 /**
@@ -26,11 +31,15 @@ void led_output( int16_t line);
  * PORTD | 5 | 7 | 1 |   |   |   | D | F |
  */
 
+uint16_t type[4] = {0x0000, 0xFFFF, 0xAAAA, 0x5555};
+uint8_t iter = 0 ;
+uint8_t counter1 = 0;
+
 void led_output( int16_t line)
 {
 	/* Read current port status and clean output bits */
 	int8_t port_b = PORTB & 0xc0;
-	int8_t port_c = PORTC & 0xd0;
+	int8_t port_c = PORTC & 0xe0;
 	int8_t port_d = PORTD & 0x1c;
 	port_d |= ( ( ( line >> 0) & 0x01) << PD5); // 1 - PD5
 	port_b |= ( ( ( line >> 1) & 0x01) << PB1); // 2 - PB1
@@ -62,32 +71,64 @@ ISR( TIMER1_COMPA_vect)
 /* Big timer interrupt. Switch on leds */
 ISR( TIMER1_COMPB_vect)
 {
-	led_output( 0xffff);
+	counter1 += 1;
+	if (!counter1)
+		iter += 1;
+	if (iter > 3)
+		iter = 0;
+	led_output( type[iter]);
 	TCNT1 = 0x00;
+}
+
+ISR( ADC_vect)
+{
+	// This section is important
+	// Disable global interrupts
+	cli(); 	
+	
+	uint16_t ADC_value;
+	
+	ADC_value = ADCL+((uint16_t) ADCH << 8);
+	
+	if ( ADC_value > Dhigh)
+		PORTD = PORTD | (_BV(PIND4));
+	if ( ADC_value < Dlow)
+		PORTD = PORTD & (~(_BV(PIND4)));
+	sei();
 }
 
 /* Initialization */
 void init()
 {
+	cli();
+	
 	/* Set output registers */
-	DDRB = 0x3f;
-	DDRC = 0x1f;
-	DDRD = 0xe3; 
+	DDRB = 0x1F;
+	DDRC = 0x1F;
+	DDRD = 0xF3;
 	
-	/* Enable global interrupts */
-	sei();
-	
+	PORTC = 0x20; //Pull up for PC5
+	PORTD = 0x0C; //Pull up for PD3,PD2
+		
 	/** Init 16bit counter **/
-	TCCR1A = 0x0;
-	TCCR1B = 0x0;
-	TIMSK = 0x0;
+	TCCR1A = 0x00;
+	TCCR1B = 0x00;
+	TIMSK = 0x00;
 	/* Set small and big match registers */
 	OCR1A = SMALL_INT;
 	OCR1B = BIG_INT;
 	/* Set prescaler value to clk/64 */
-	TCCR1B |= ( ( 1 << CS11) | ( 1 << CS10));
+	TCCR1B |= ( ( 1 << CS11) | ( 0 << CS10));
 	/* Enable counter compare interrupts */
 	TIMSK |= ((1 << OCIE1A) | ( 1 << OCIE1B));
+	
+	// ADC initialization
+	ADMUX = 0x05; //AREF as source, ADC5(PC5) as input source
+	ADCSRA = 0xFF; // Enable ADC interrupt, set clock multiplexer to 128
+	
+	/* Enable global interrupts */
+	sei();
+	
 }
 
 int main( void)
